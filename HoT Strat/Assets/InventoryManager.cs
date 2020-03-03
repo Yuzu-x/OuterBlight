60c725e1b61e03daebee21303261bee946b068c8
Yuzu-x <54381207+Yuzu-x@users.noreply.github.com>
2020-03-03 15:57:10 +1100

    Repaired

---
+++ HoT Strat/Assets/InventoryManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public Transform invParent;
+
+    CharacterInventory myItems;
+
+    ItemSlotManager[] slots;
+
+    void Start()
+    {
+        myItems = CharacterInventory.instance;
+        myItems.onItemChangeCallback += UpdateInventory;
+
+        slots = invParent.GetComponentsInChildren<ItemSlotManager>();
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void UpdateInventory()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if(i < myItems.items.Count)
+            {
+                slots[i].AddItem(myItems.items[i]);
+
+            }
+            else
+            {
+                slots[i].EmptySlot();
+
+            }
+        }
+    }
+}
