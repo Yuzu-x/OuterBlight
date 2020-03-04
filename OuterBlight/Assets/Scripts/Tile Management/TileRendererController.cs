a8cd50cd33714fd2d5bf50bc682936d382cd60e5
Yuzu-x <54381207+Yuzu-x@users.noreply.github.com>
2020-03-04 16:15:05 +1100

    Added tile scripts

---
+++ OuterBlight/Assets/Scripts/Tile Management/TileRendererController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileRendererController : MonoBehaviour
+{
+    public bool enabledRenderer = false;
+    public GameObject[] findTiles;
+    List<GameObject> tileList = new List<GameObject>();
+
+    public void Start()
+    {
+        findTiles = GameObject.FindGameObjectsWithTag("Tile");
+
+
+        foreach (GameObject gO in findTiles)
+        {
+            gO.GetComponent<MeshRenderer>().enabled = false;
+            tileList.Add(gO);
+        }
+    }
+
+   public void EnableOrDisableRenderer()
+    {
+        if (!enabledRenderer)
+        {
+            foreach (GameObject tile in tileList)
+            {
+                tile.GetComponent<MeshRenderer>().enabled = true;
+            }
+
+            enabledRenderer = true;
+        }
+        else if (enabledRenderer)
+        {
+            foreach (GameObject tile in tileList)
+            {
+                tile.GetComponent<MeshRenderer>().enabled = false;
+            }
+
+            enabledRenderer = false;
+        }
+    }
+}
